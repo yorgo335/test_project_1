d8446598d1db545d57763a5885d3744b60885f9e
yorgo335 <yorgonassar04@gmail.com>
2025-06-16 17:50:21 +0300

    modified:   Style.css 	modified:   aboutus.html 	modified:   contactus.html 	modified:   index.html 	modified:   myScripts.js 	modified:   store.html

--- myScripts.js
+++ myScripts.js
@@ -1,35 +1,42 @@
-/*do note both functions will be used in every page as dark/light mode is universal*/
+/* I did not remove any old codes because they will a 100% help me understand the thought process incase i forget something in the newer codes */
 
-
-/*logic here is that body tags onload will call this function and pass its id, then it will check if the saved theme is Dark
-and apply it else if it was white or if there was no saved theme then will just set to Light mode*/
-function CheckSavedTheme(bodyid){
-	const tag = document.getElementById(bodyid);
-	const imgTag = document.getElementById("DarkLightImg")
-	if(localStorage.getItem("theme") == "Dark"){
-		tag.className = "Dark";
-		imgTag.src = "assets/light-mode.png";
-	}else{
-		tag.className = "Light";
-		imgTag.src = "assets/dark-mode.png";
-	}
-}
-
-/*logic here is that we send the body's id, check its current class(Light or Dark) and will then change it to the opposite class 
-and update the image. Finally we will locally store the theme so we the theme remains the same even after refreshing the page.*/
-function toggleDarkLight(bodyid) { 
-	const tag = document.getElementById(bodyid);
+function toggleDarkLight() { 
+	const currentTheme= document.documentElement.getAttribute('data-theme');
 	const imgTag = document.getElementById("DarkLightImg");
-	if(tag.className == "Dark"){
-		tag.className = "Light";
+	if(currentTheme == "dark"){
+		document.documentElement.removeAttribute('data-theme');
 		imgTag.src = "assets/dark-mode.png";
-		localStorage.setItem("theme", "Light");
+		localStorage.setItem("theme", "light");
 	}else{
-		tag.className = "Dark";
+		document.documentElement.setAttribute('data-theme', 'dark');
 		imgTag.src = "assets/light-mode.png";
-		localStorage.setItem("theme", "Dark");
+		localStorage.setItem("theme", "dark");
 	}
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	const imgTag = document.getElementById("DarkLightImg")
+	if(localStorage.getItem("theme") == "dark"){
+		document.documentElement.setAttribute('data-theme', 'dark');
+		imgTag.src = "assets/light-mode.png";
+	}else{
+		imgTag.src = "assets/dark-mode.png";
+	}
+});
+
+/*alternatively i could make each body tag have onload="CheckSavedTheme()" and run the function below
+
+function CheckSavedTheme(){
+	const imgTag = document.getElementById("DarkLightImg")
+	if(localStorage.getItem("theme") == "dark"){
+		document.documentElement.setAttribute('data-theme', 'dark');
+		imgTag.src = "assets/light-mode.png";
+	}else{
+		imgTag.src = "assets/dark-mode.png";
+	}
+}*/
+
 
 function loadProducts () {
 	/*const items = [{name:"Beats Studio Pro", price:199.99, imagepath:"assets/Beats-Studio-Pro.jpg"}, {name:"Sony LinkBuds", price:128.99, imagepath:"assets/Sony-LinkBuds.jpg"}];
@@ -56,3 +63,43 @@ function loadProducts () {
   	})
 
 }
+
+
+
+/* UPDATE: code below is outdated, used to use body's id and stuff and setting the body color and bg color but now i am using
+   document.documentElement.set/removeAttribute alongside data-theme to basically make it so that by default its light and you can toggle
+   between dark and light*/
+
+
+/*do note both functions will be used in every page as dark/light mode is universal*/
+
+
+/*logic here is that body tags onload will call this function and pass its id, then it will check if the saved theme is Dark
+and apply it else if it was white or if there was no saved theme then will just set to Light mode*/
+/*function CheckSavedTheme(bodyid){
+	const tag = document.getElementById(bodyid);
+	const imgTag = document.getElementById("DarkLightImg")
+	if(localStorage.getItem("theme") == "Dark"){
+		tag.className = "Dark";
+		imgTag.src = "assets/light-mode.png";
+	}else{
+		tag.className = "Light";
+		imgTag.src = "assets/dark-mode.png";
+	}
+}*/
+
+/*logic here is that we send the body's id, check its current class(Light or Dark) and will then change it to the opposite class 
+and update the image. Finally we will locally store the theme so we the theme remains the same even after refreshing the page.*/
+/*function toggleDarkLight(bodyid) { 
+	const tag = document.getElementById(bodyid);
+	const imgTag = document.getElementById("DarkLightImg");
+	if(tag.className == "Dark"){
+		tag.className = "Light";
+		imgTag.src = "assets/dark-mode.png";
+		localStorage.setItem("theme", "Light");
+	}else{
+		tag.className = "Dark";
+		imgTag.src = "assets/light-mode.png";
+		localStorage.setItem("theme", "Dark");
+	}
+}*/
